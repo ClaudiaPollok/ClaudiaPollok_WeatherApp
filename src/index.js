bdf010952c4dff9fd94c513d3cb7230912b65701
ClaudiaPollok <pollokclaudia@aol.com>
2025-01-09 14:39:04 +0100

    Added search function with API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let currentTempElement = document.querySelector("#currentTemp");
+  let currentTemp = response.data.temperature.current;
+  currentTempElement.innerHTML = Math.round(currentTemp);
+  let cityElement = document.querySelector("#cityName");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "5t6ddf5ab847cfob04fcc84f5baa13c0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#cityName");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Berlin");
